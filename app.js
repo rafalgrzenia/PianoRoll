1eee2685e62e82e5dd8e8d4824b14addbd087c76
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-10-31 17:11:38 +0100

    Add containers and update openPianoRoll method

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+import PianoRoll from "./pianoroll.js";
+
 const pianoRollContainer = document.querySelector(".pianoRollContainer");
 const pianoMainContainer = document.querySelector(".piano-main-container");
 const pianorollMain = document.querySelector(".pianoroll-main");
 const pianorollSide = document.querySelector(".pianoroll-side");
-
 
 class PianoRollDisplay {
   constructor(csvURL) {
@@ -12,14 +13,57 @@ class PianoRollDisplay {
 
   async loadPianoRollData() {
     try {
-      const response = await fetch('https://pianoroll.ai/random_notes');
+      const response = await fetch("https://pianoroll.ai/random_notes");
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       this.data = await response.json();
     } catch (error) {
-      console.error('Error loading data:', error);
+      console.error("Error loading data:", error);
     }
+  }
+
+  openPianoRoll(rollId) {
+    // Clear Main container
+
+    pianorollMain.innerHTML = "";
+
+    // Select all piano roll cards from roll pianoRollContainer
+
+    const pianoRolls = Array.from(
+      pianoRollContainer.querySelectorAll(".piano-roll-card")
+    );
+
+    // Find clicked element and append it to Main Container
+
+    pianoRolls.find((el) => {
+      const clone = el.cloneNode(true);
+      const id = Number(el.getAttribute("id"));
+      if (id === rollId) {
+        pianorollMain.append(clone);
+      }
+    });
+
+    // Show PianoMainContainer and Hide pianoRollContainer
+
+    pianoMainContainer.classList.add("show");
+
+    pianoRollContainer.classList.add("hide");
+
+    // Clone PianoRolls, add them to side container handle event listener that adds clicked element to main view
+
+    const clonedPianoRolls = [...pianoRolls];
+
+    clonedPianoRolls.forEach((pianoroll) => {
+      pianoroll.addEventListener("click", () => {
+        pianorollMain.innerHTML = "";
+        clonedPianoRolls.forEach((roll) => roll.classList.remove("active"));
+        const cloneNode = pianoroll.cloneNode(true);
+        pianorollMain.append(cloneNode);
+        pianoroll.classList.add("active");
+      });
+      pianorollSide.append(pianoroll);
+    });
   }
 
   preparePianoRollCard(rollId) {
@@ -31,6 +75,9 @@ class PianoRollDisplay {
     descriptionDiv.classList.add('description');
     descriptionDiv.textContent = `This is a piano roll number ${rollId}`;
     cardDiv.appendChild(descriptionDiv);
+    cardDiv.addEventListener("click", () => {
+      this.openPianoRoll(rollId);
+    });
 
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.classList.add('piano-roll-svg');
